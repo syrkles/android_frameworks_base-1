9f36797ccf4cb753d20fad6616e9d9c191d2577b
ezio84 <brabus84@gmail.com>
2018-11-01 21:21:15 +0100

    Pulse: add back auto color based on albumart
    added also this:
    https://github.com/ezio84/abc_frameworks_base/commit/8ec40a05f783deea376ae0ee3fa71aa599e9a898
    to not skip notifications if blocked on lockscreen
    
    Change-Id: I4bbb8f9988ba3fe81b1a1e1b7d4426209b4d363c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
@@ -743,6 +743,20 @@ public class NavigationBarFragment extends Fragment implements Callbacks, Keygua
         }
     }
 
+    public void setPulseColors(boolean colorizedMedia, int[] colors) {
+        if (mNavigationBarView != null) {
+            mNavigationBarView.setPulseColors(colorizedMedia, colors);
+        }
+    }
+
+
+    @Override
+    public void onMediaUpdated(boolean playing) {
+        if (mNavigationBarView != null) {
+            mNavigationBarView.setMediaPlaying(playing);
+        }
+    }
+
     // ----- Internal stuffz -----
 
     private void refreshLayout(int layoutDirection) {
@@ -796,13 +810,6 @@ public class NavigationBarFragment extends Fragment implements Callbacks, Keygua
     public void onDetach() {
         mNavigationBarView.dispose();
         super.onDetach();
-    }
-
-    @Override
-    public void onMediaUpdated(boolean playing) {
-        if (mNavigationBarView != null) {
-            mNavigationBarView.setMediaPlaying(playing);
-        }
     }
 
     private void prepareNavigationBarView() {
